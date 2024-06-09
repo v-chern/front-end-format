e6787e428c632cc93a5ad1fc8fae41355c67a6d8
v-chern <vladimir.v.chernikov@yandex.ru>
2024-06-09 13:22:57 +0200

    gulp images processing fix

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,14 +1,22 @@
-const gulp = require('gulp');
+import gulp from 'gulp';
+import concat from 'gulp-concat-css';
+import plumber from 'gulp-plumber';
+import imagemin from 'gulp-imagemin';
+import del from 'del';
+import browserSync from 'browser-sync';
+
+/*const gulp = require('gulp');
 const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
-const del = require('del');
-const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const del = require('del');*/
+const bs = browserSync.create();
 
 function html() {
     return gulp.src('src/**/*.html')
                 .pipe(plumber())
                 .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true}));
+                .pipe(bs.reload({stream: true}));
 }
 
 function css() {
@@ -16,19 +24,26 @@ function css() {
                 .pipe(plumber())
                 .pipe(concat('bundle.css'))
                 .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true}));
+                .pipe(bs.reload({stream: true}));
 }
 
 function fonts() {
     return gulp.src('src/fonts/**/*.{css,woff,woff2}')
                 .pipe(gulp.dest('dist/fonts/'))
-                .pipe(browserSync.reload({stream: true}));
+                .pipe(bs.reload({stream: true}));
 }
 
 function images() {
-    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', {encoding: false})
+                .pipe(imagemin())
+                .on('error', (err) => {
+                    console.error('Error in imagemin:', err.toString());
+                })
                 .pipe(gulp.dest('dist/images'))
-                .pipe(browserSync.reload({stream: true}));
+                .pipe(bs.reload({stream: true}))
+                .on('end', () => {
+                    console.log('Images process successfully');
+                });
   }
 
 function clean() {
@@ -53,6 +68,10 @@ function serve() {
 const build = gulp.series(clean, gulp.parallel(html, css, fonts, images));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
+export { build };
+export default watchapp;
+
+/*
 exports.build = build;
 
-exports.default = watchapp;
+exports.default = watchapp;*/
